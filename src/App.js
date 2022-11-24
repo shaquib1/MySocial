d280ba56bd212d8e94cbd6416dc26277adde5e22
Md Shaquib Alam <shaquib475@gmail.com>
2022-11-24 14:21:41 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./pages/home/Home";
+//import Login from "./pages/login/Login";
+//import Profile from "./pages/profile/Profile";
+//import Register from "./pages/register/Register";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
